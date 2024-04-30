29f5b7c87391af1c7bec9e3aae2c56273d7dfc4f
Daniel Casita <legendadtwo@gmail.com>
2024-04-29 20:49:56 -0500

    Version estable, con Favicon, falta diseño en figma para empezar a aplicarle css a gusto, se crea nueva rama de desarrollo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,11 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Mi Clima",
   description: "App del Clima en Next Js - Prueba Frontend",
+  icons:{
+    icon:['/favicon.ico?v=4'],
+    apple:['/apple-touch-icon.png?v=4'],
+    shortcut:['/apple-touch-icon.png']
+  }
 };
 
 export default function RootLayout({
@@ -16,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className} style={{ margin: 0 }}>{children}</body>
     </html>
   );
 }
