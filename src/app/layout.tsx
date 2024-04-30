cb25a3bbe1a0e941d09c8dbe602527941816c099
Daniel Casita <legendadtwo@gmail.com>
2024-04-29 20:58:34 -0500

    Version estable, cambié tipografía a poppins, empiezo a trabajar en el diseño de figma

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Mi Clima",
@@ -21,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className} style={{ margin: 0 }}>{children}</body>
+      <body style={{ margin: 0, fontFamily: "'Poppins', sans-serif" }}>
+        {children}
+      </body>
     </html>
   );
 }
