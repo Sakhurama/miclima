8b3154c46d7ea08b666078a57345520bca7c6861
Daniel Casita <legendadtwo@gmail.com>
2024-10-22 23:02:06 -0500

    Solución al responsive y agrego bienvenida

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
 import styles from "./styles/page.module.css";
-import './styles/animation/loading.css'
-import SearchIcon from './components/SearchIcon';
 import ContactForm from './components/ContactForm';
 
 // Definimos una interfaz para los datos del clima
@@ -69,13 +67,13 @@ export default function Home() {
           <input
             className={styles.input_field}
             type="text"
-            placeholder="Consulte aquí su ciudad"
+            placeholder="Escriba una ciudad..."
             id="cityName"
             name="cityName"
             onChange={(e) => setCity(e.target.value)}
           />
           <button className={styles.searchbtn} type="submit">
-            <SearchIcon />
+            <svg width="55" height="55" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M18.319 14.4326C20.7628 11.2941 20.542 6.75347 17.6569 3.86829C14.5327 0.744098 9.46734 0.744098 6.34315 3.86829C3.21895 6.99249 3.21895 12.0578 6.34315 15.182C9.22833 18.0672 13.769 18.2879 16.9075 15.8442C16.921 15.8595 16.9351 15.8745 16.9497 15.8891L21.1924 20.1317C21.5829 20.5223 22.2161 20.5223 22.6066 20.1317C22.9971 19.7412 22.9971 19.1081 22.6066 18.7175L18.364 14.4749C18.3493 14.4603 18.3343 14.4462 18.319 14.4326ZM16.2426 5.28251C18.5858 7.62565 18.5858 11.4246 16.2426 13.7678C13.8995 16.1109 10.1005 16.1109 7.75736 13.7678C5.41421 11.4246 5.41421 7.62565 7.75736 5.28251C10.1005 2.93936 13.8995 2.93936 16.2426 5.28251Z" fill="currentColor" /></svg>
           </button>
         </form>
 
@@ -111,6 +109,7 @@ export default function Home() {
                 <br />
               </div>
             </div>
+
             <div className={styles.place}>{weatherData.name}</div>
             <div className={styles.containerForm}>
               {/* Botón para abrir el pop-up */}
@@ -132,9 +131,9 @@ export default function Home() {
             </div>
           </>
         ) : (
-          // Loader icono de carga
-          <div className={styles.place}>
-            <div className="lds-facebook"><div></div><div></div><div></div></div>
+          <div className={styles.bienvenida}>
+            <p>¡BIENVENIDO!</p>
+            <span>Consulte el clima de cualquier ciudad</span>
           </div>
         )}
       </article>
